b95d1130043aa06634c01354f5bc9d213d1426a3
Yuki <admin@yuki.sh>
2026-02-25 11:16:12 +0800

    chore: add prettier and configuration

---
+++ prettier.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'prettier';
+
+export default {
+  tabWidth: 2,
+  semi: true,
+  endOfLine: 'lf',
+  singleQuote: true,
+  printWidth: 100,
+  vueIndentScriptAndStyle: true,
+} satisfies Config;
